51152f7247db63e1bab966e59eae8ee58cae314e
Tobias-PC\Tobias <buttermilchschoppen@gmx.de>
2017-09-16 08:25:10 +0200

    Es werden jetz alle Bilder aus einem Ordner geladen und Klassifiziert, Session wird nur einmal gestartet und dann an die Funktion zum Klassifizieren übergeben. Dadurch dauert die Bewertung je nach Model 0,4-0,45sec.
    Komisch ist dass die erste Bewertung über 3sec. dauert

--- Bilder-Sortierung/Classify/Classify_and_Output.py
+++ Bilder-Sortierung/Classify/Classify_and_Output.py
@@ -19,13 +19,13 @@ import time
 Setup environment (e.g. tf model, path to image files)
 """
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2' # turn down log-level to suppress insignificant warning messages
-label_directory="../../../30000_InceptionV3/retrained_labels.txt"
-graph_directory="../../../30000_InceptionV3/retrained_graph.pb"
+label_directory="/tf/30000_InceptionV3/output_labels.txt"
+graph_directory="/tf/30000_InceptionV3/output_graph.pb"
 input_layer='DecodeJpeg/contents:0'
 output_layer= 'final_result:0'
 top_predictions=5
 pictureArray=[]
-picture_directory='../../../ValidationSet_debug'
+picture_directory='/tmp\ValidationSet/'
 
 def load_image(filename):
   """Read in the image_data to be classified."""
@@ -57,49 +57,84 @@ def direction_to_number(arg):
 arg: binary image file
 return: integer value of tf class
 """
-def getRopePosition(image): # TODO speed up the image classification
-    with tf.Session() as sess:
-        softmax_tensor = sess.graph.get_tensor_by_name(output_layer)
-        labels = load_labels(label_directory)
-        tmp = 0
-        direction = ""
-        predictions, = sess.run(softmax_tensor, {input_layer: image})
+def getRopePosition(image,Session, outputLayer, labels): # TODO speed up the image classification --> Done!
 
-        # Sort to show labels in order of confidence
-        top_k = predictions.argsort()[-top_predictions:][::-1]
-        for node_id in top_k:
-            human_string = labels[node_id]
-            score = predictions[node_id]
+    tmp = 0
+    direction = ""
+    predictions, = Session.run(outputLayer, {input_layer: image})
 
-            if (tmp < score):
-                tmp = score
-                direction = human_string
+    # Sort to show labels in order of confidence
+    top_k = predictions.argsort()[-top_predictions:][::-1]
+    for node_id in top_k:
+        human_string = labels[node_id]
+        score = predictions[node_id]
 
-        direction_number=direction_to_number(direction)
+        if (tmp < score):
+            tmp = score
+            direction = human_string
+
+    direction_number=direction_to_number(direction)
     return direction_number
+
+
+"""
+arg:Outputlayer to Load, Session-Object
+return: integer value of tf class
+"""
+def getOutputLayer(OutputLayer, Session):
+    softmax_tensor = Session.graph.get_tensor_by_name(OutputLayer)
+    return softmax_tensor
+
+def startSession():
+    with tf.Session() as sess:
+        return sess
+
+def getAllPictures(Path):
+    return os.listdir(Path)
+
 
 # test the classification
 def main(argv):
-  # load tf labels
-  #labels = load_labels(label_directory)
-  start = time.time()
-  # load tf graph, which is stored in the default session
-  load_graph(graph_directory)
-  print('load_graph elapsed time (sec): %s' % (time.time() - start))
 
-  # test classification with single image files
-  start = time.time()
-  image_data = load_image('HalbLinks (1).jpg')
-  dir_number=getRopePosition(image_data)
-  print(dir_number)
-  print('classify first image elapsed time (sec): %s' % (time.time() - start))
+    pictureArray = getAllPictures(picture_directory)
+    # load tf labels
+    labels = load_labels(label_directory)
+    start = time.time()
+    # load tf graph, which is stored in the default session
+    load_graph(graph_directory)
+    print('load_graph elapsed time (sec): %s ' % (time.time() - start))
 
-  start = time.time()
-  image_data = load_image('Rechts (7).jpg')
-  dir_number=getRopePosition(image_data)
-  print(dir_number)
-  print('classify second image elapsed time (sec): %s' % (time.time() - start))
+    #Load Graph once
+    # start = time.time()
+    session = startSession()
+    #print(' elapsed time (sec): %s' % (time.time() - start))
+
+    #Load outputLayer once
+    #start = time.time()
+    outputTensor= getOutputLayer(output_layer, session)
+    #print('getOutputLayer elapsed time (sec): %s' % (time.time() - start))
+
+    # test classification with single image files
+
+    for image in pictureArray:
+
+        start = time.time()
+        image_data = load_image(image)
+        print('load_image elapsed time (sec): %s' % (time.time() - start))
+
+        start = time.time()
+        dir_number=getRopePosition(image_data, session,outputTensor, labels)
+        print("Prediction: %i " % dir_number)
+        print('classify image elapsed time (sec): %s' % (time.time() - start))
+        print()
+
+
+
 
 # what is going on here?
+#-> das sorgt dafür dass wenn es sich hier um eine Klasse oder ein in einem Projekt
+# verwendetes Skript handelt, man dieses auch sozusagen einzeln testen kann. Es wird quasi eine
+# Main-Methode in diesem Skript ausgeführt und nicht beim starten die Main des Projekts.
+# Zumindest versteh ich das so...
 if __name__ == '__main__':
   tf.app.run(main=main)
